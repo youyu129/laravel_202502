84f439bcba754d40489107ec34600efb399bb054
youyu <129youyu@gmail.com>
2025-02-25 13:37:09 +0800

    新增laravel專案 car food

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,72 @@
 <?php
-
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('index');
+});
+
+// 取各種名字 可以到同一個view
+// food
+Route::get('/', function () {
+    return redirect()->route('foods.index');
+});
+Route::get('/food', function () {
+    return redirect()->route('foods.index');
+});
+
+// car
+Route::get('/', function () {
+    return redirect()->route('cars.index');
+});
+Route::get('/car', function () {
+    return redirect()->route('cars.index');
+});
+
+Route::get('/youyu', function () {
+    return redirect()->route('cars.index');
+});
+
+// 取路由小名
+// food
+Route::get('/foods', function () {
+    return view('food.index');
+})->name('foods.index');
+
+Route::get('/foods/f1', function () {
+    return view('food.f1');
+})->name('foods.f1');
+
+Route::get('/foods/f2', function () {
+    return view('food.f2');
+})->name('foods.f2');
+
+Route::get('/foods/f3', function () {
+    return view('food.f3');
+})->name('foods.f3');
+
+// car
+// Route::get('/cars', function () {
+//     return view('car.index');
+// })->name('cars.index');
+
+// Route::get('/cars/create', function () {
+//     return view('car.create');
+// })->name('cars.create');
+
+// Route::get('/cars/update', function () {
+//     return view('car.update');
+// })->name('cars.update');
+
+// Route::get('/cars/del', function () {
+//     return view('car.del');
+// })->name('cars.del');
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,5 +87,9 @@ Route::get('/students/create', function () {
 Route::get('/students/edit', function () {
     return view('student.edit');
 })->name('students.edit');
+
+Route::resource('cars', CarController::class);
+
+Route::resource('tests', TestController::class);
 
 Route::resource('students', StudentController::class);
